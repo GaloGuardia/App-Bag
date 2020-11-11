de9dd57913a2e4590144c413dbf3f9715845f640
Galo <galoguardia@gmail.com>
2020-11-11 02:36:17 -0300

    Finalizado: CRUD de base de datos

--- src/static/js/databaseDAO.js
+++ src/static/js/databaseDAO.js
@@ -10,17 +10,8 @@ class DatabaseDAO {
         return await this.db.insert(data);
     }
 
-    async update(dataToUpdate, dataToReplace) {
-        // dataToReplace debe contener toda la linea a modificar, usar find(dataToUpdate).
-        return await this.db.update(dataToUpdate, dataToReplace, {}).then((dataUpdated) => {
-            // numReplaced = 1
-            // The doc #3 has been replaced by { _id: 'id3', planet: 'Pluton' }
-            // Note that the _id is kept unchanged, and the document has been replaced
-            // (the 'system' and inhabited fields are not here anymore)
-            return dataUpdated;
-        }).catch((err) => {
-            alert(err);
-        });
+    async update(dataToUpdate, newData) {
+        return await this.db.update(dataToUpdate, newData, {});
     }
 
     async find(data) {
@@ -31,13 +22,8 @@ class DatabaseDAO {
         return await this.db.count(data);
     }
 
-    async remove(id) {
-        return await this.db.remove({ _id: id }, {}).then((value) => {
-            // value = 1
-            return value;
-        }).catch((err) => {
-            alert(err);
-        });
+    async remove(username) {
+        return await this.db.remove({ username: username }, {});
     }
 }
 
